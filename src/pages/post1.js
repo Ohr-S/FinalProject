acc3df52df31562f003e111a7be240dd2b887359
Ohr Shmueli <73228323+Ohr-S@users.noreply.github.com>
2023-05-14 10:05:53 +0300

    hw5

--- src/pages/post1.js
+++ src/pages/post1.js
@@ -2,10 +2,11 @@ import React from "react";
 
 
 export default function PostView({post}){
-console.log(post)
+
     return(
 
         <div>
+            <h2>{post.title}<br/> {post.content}<br/> {post.published}<br/> {post.author}}</h2>
         </div>
     )
 
