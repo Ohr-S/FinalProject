5378644d37c73ba598e76bac973d38c16d465542
Ohr Shmueli <73228323+Ohr-S@users.noreply.github.com>
2023-07-01 20:46:51 +0300

    Updating dump to include comments

--- sql_dump.sql
+++ sql_dump.sql
@@ -16,6 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` date NOT NULL,
+  `user_id` int NOT NULL,
+  `post_id` int NOT NULL,
+  `content` varchar(1024) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` VALUES (7,'2023-07-01',642217,7,'WOOOWWWW'),(8,'2023-07-01',642218,7,'THIS IS MEEE');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `posts`
 --
 
@@ -29,7 +60,7 @@ CREATE TABLE `posts` (
   `user_id` int DEFAULT NULL,
   `created_at` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +69,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (7,'IDODO','Default Value besttt',642217,'2023-07-01');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +92,7 @@ CREATE TABLE `sessions` (
 
 LOCK TABLES `sessions` WRITE;
 /*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('Y9P9sZm5SB5XV8hP7kPCGEVjlIgEB4V2',642214),('YzWX3HWoCdspSTejtOD6elBMB4QzUZ9e',642214),('YVRJ1CFmEWHkag2J0tTaItrWQSxbfTfH',642214),('r3sTLQBcPTETXxXNEZTL4GIM6HNYnapz',642214),('MuKtQAcB3N5ahGGj7nqjCGEQyv4WD7Zy',642214);
+INSERT INTO `sessions` VALUES ('ph76PpzD4XumETK4DZpSPDZx6mYVGw0M',642218);
 /*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -75,9 +107,8 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(64) DEFAULT NULL,
   `password` binary(64) DEFAULT NULL,
-  `salt` binary(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=642215 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=642219 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,7 +117,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (642214,'ohr',_binary '$2b$12$fv4cJRxqIS8smLLl7g8s6eY5Cfrx96uGiEkUN4ZaV2NzM6YM/Ho1G\0\0\0\0',_binary '$2b$12$fv4cJRxqIS8smLLl7g8s6e\0\0\0');
+INSERT INTO `users` VALUES (642217,'admin',_binary '$2b$12$Eb1P0zJ.CL1aJgwXEBUddugOv7krbCD243b/NEvVuCrSEcDmG.KDK\0\0\0\0'),(642218,'ido',_binary '$2b$12$guJR5bPn5BTb2PgKgwHRG.i3398YYnPWDMED.I0zppu8SK1R0moY6\0\0\0\0');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -99,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-09 17:50:15
+-- Dump completed on 2023-07-01 20:46:08
